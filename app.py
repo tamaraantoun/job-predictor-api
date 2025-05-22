db9eb2364a7c8bd77578614c10b9b01706963b3d
tamaraantoun <tamantoun@gmail.com>
2025-05-22 18:04:14 +0200

    scnd commit

--- app.py
+++ app.py
@@ -26,4 +26,6 @@ def home():
     return "✅ Job Prediction API is running!"
 
 if __name__ == "__main__":
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
